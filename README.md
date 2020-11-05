155470e627b32f9fea296074dfb22994181e171e
nihui <shuizhuyuanluo@126.com>
2020-11-06 00:38:05 +0800

    ncnn android yolov5

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ncnn-android-yolov5
-The YOLOv5 object detection android example 
+
+The YOLOv5 object detection
+
+this is a sample ncnn android project, it depends on ncnn library only
+
+https://github.com/Tencent/ncnn
+
+## how to build and run
+### step1
+https://github.com/Tencent/ncnn/releases
+
+download ncnn-android-vulkan-lib.zip or build ncnn for android yourself
+
+### step2
+extract ncnn-android-vulkan-lib.zip into app/src/main/jni or change the ncnn path to yours in app/src/main/jni/CMakeLists.txt
+
+### step3
+open this project with Android Studio, build it and enjoy!
+
+## screenshot
+![](screenshot.jpg)
+
---
+++ app/src/main/java/com/tencent/yolov5ncnn/YoloV5Ncnn.java
@@ -0,0 +1,39 @@
+// Tencent is pleased to support the open source community by making ncnn available.
+//
+// Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+package com.tencent.yolov5ncnn;
+
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+
+public class YoloV5Ncnn
+{
+    public native boolean Init(AssetManager mgr);
+
+    public class Obj
+    {
+        public float x;
+        public float y;
+        public float w;
+        public float h;
+        public String label;
+        public float prob;
+    }
+
+    public native Obj[] Detect(Bitmap bitmap, boolean use_gpu);
+
+    static {
+        System.loadLibrary("yolov5ncnn");
+    }
+}
